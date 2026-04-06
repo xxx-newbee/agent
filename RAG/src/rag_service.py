a19d8653dfce09d47fdd284e3458274b45f04c10
xxx <huang_learn@163.com>
2026-04-06 12:07:21 +0800

    test stream

--- RAG/src/rag_service.py
+++ RAG/src/rag_service.py
@@ -5,28 +5,7 @@ import embedding
 import vectorstore
 from openai import OpenAI
 
-llm_client = OpenAI(api_key="ollama", base_url="http://localhost:11434/v1")
-
-def generate_answer(question: str):
-    context = retrieve_context(question)
-
-    prompt = f"""
-    你是一个智能助手。请根据以下提供的上下文信息来回答用户的问题。如果无法从上下文中找到答案，请直接说明。
-
-    <context>
-    {context}
-    /context>
-
-    <question>
-    {question}
-    </question>
-    """
-
-    response = llm_client.chat.completions.create(
-        model="qwen3.5:9b",
-        messages=[{"role":"user", "content": prompt}]
-    )
-    return response.choices[0].message.content
+llm_client = OpenAI(api_key="who care", base_url="http://localhost:11434/v1")
 
 def retrieve_context(question: str, top_k: int = 3):
     query_embedding = embedding.get_embedding(question)
@@ -45,5 +24,29 @@ def retrieve_context(question: str, top_k: int = 3):
     return context.strip()
 
 
-answer = generate_answer("Nexus 的工作原理是怎样的？")
-print(answer)
+if __name__ == "__main__":
+    question = "什么是RAG？"
+    context = retrieve_context(question)
+
+    prompt = f"""
+    你是一个智能助手。请根据以下提供的上下文信息来回答用户的问题。如果无法从上下文中找到答案，请直接说明。
+
+    <context>
+    {context}
+    /context>
+
+    <question>
+    {question}
+    </question>
+    """
+
+    response = llm_client.chat.completions.create(
+        model="qwen/qwen3.5-9b",
+        messages=[{"role":"user", "content": prompt}],
+        stream=True
+    )
+    for chunk in response:
+        content = chunk.choices[0].delta.content
+        if content:
+            print(content, end="", flush=True)
+    print()
